ef7d9f230a8d5bdc9c88fc869c961d1ebf6e9051
Wawrzyniec87 <wawrzyniec20@gmail.com>
2022-04-20 19:42:29 +0200

    Now upon enemy hit player's sprite flashes and the player is invincible for two seconds

--- Assets/Scripts/Player/PlayerHealthHandler.cs
+++ Assets/Scripts/Player/PlayerHealthHandler.cs
@@ -8,6 +8,11 @@ public class PlayerHealthHandler : MonoBehaviour
     [SerializeField] int currentHealth;
     [SerializeField] int maxHealth;
 
+    [SerializeField] float invincibilityTime = 1f;
+    private bool isInvincible;
+
+    [SerializeField] SpriteRenderer playerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,24 +21,64 @@ public class PlayerHealthHandler : MonoBehaviour
         UIManager.instance.healthSlider.maxValue = maxHealth;
 
         PlayerHealthUpdate();
-    }
 
-    private void Update()
-    {
-        PlayerHealthUpdate();
+        isInvincible = false;
     }
 
     public void DamagePlayer(int damageTaken)
     {
-        currentHealth -= damageTaken;
-        PlayerHealthUpdate();
-
-        if(currentHealth <= 0)
+        if (!isInvincible)
         {
-            gameObject.SetActive(false);
-            UIManager.instance.DisplayDeathScreen();
-        }
+            currentHealth -= damageTaken;
+            PlayerHealthUpdate();
 
+            MakePlayerInvincible();
+
+            if (currentHealth <= 0)
+            {
+                gameObject.SetActive(false);
+                UIManager.instance.DisplayDeathScreen();
+            }
+        }
+    }
+
+    public void MakePlayerInvincible()
+    {
+        isInvincible = true;
+
+        StartCoroutine(Flasher());
+        StartCoroutine(PlayerNotInvincible());
+    }
+
+    IEnumerator Flasher()
+    {
+        for(int i=0; i < 5; i++)
+        {
+            playerSprite.color = new Color(
+                playerSprite.color.r,
+                playerSprite.color.g,
+                playerSprite.color.b,
+                0.1f
+                );
+
+            yield return new WaitForSeconds(.1f);
+
+            playerSprite.color = new Color(
+                playerSprite.color.r,
+                playerSprite.color.g,
+                playerSprite.color.b,
+                1f
+                );
+
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+    
+    IEnumerator PlayerNotInvincible()
+    {
+        yield return new WaitForSeconds(invincibilityTime);
+
+        isInvincible = false;
     }
 
     public void PlayerHealthUpdate()
